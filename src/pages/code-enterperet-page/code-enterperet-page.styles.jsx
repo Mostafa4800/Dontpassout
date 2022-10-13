141ee6d6587290f5017a5633351a1af12367fb5e
Mostafa4800 <68566506+Mostafa4800@users.noreply.github.com>
2022-10-13 10:52:45 +0200

    Add files via upload

--- src/pages/code-enterperet-page/code-enterperet-page.styles.jsx
+++ src/pages/code-enterperet-page/code-enterperet-page.styles.jsx
@@ -67,68 +67,69 @@ margin-top:150px;
 `
 
 export const PopUpBackGround = styled.div`
-    height: ${(props) => props.active === "active" ? "100%"  : "0"};
-    width: 100%;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    background-color: rgb(6, 7, 33, .8);
-    overflow-x: hidden;
-    transition: 0.5s;
-    justify-content:center;
-`
-//faillure popup
-export const PopUpPos = styled.div`
-text-align: center;
-display: flex;
-justify-content: center;
-align-items: center;
-text-decoration: none;
-height: 100%;
-width: 100%;
+     height: ${(props) => props.active === "active" ? "100%"  : "0"};
+     width: 100%;
+     position: fixed;
+     z-index: 1;
+     top: 0;
+     left: 0;
+     background-color: rgb(6, 7, 33, .8);
+     overflow-x: hidden;
+     transition: 0.5s;
+     justify-content:center;
+ `
+ //faillure popup
+ export const PopUpPos = styled.div`
+ text-align: center;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ text-decoration: none;
+ height: 100%;
+ width: 100%;
 
-@media screen and (max-width: 520px) {
-    transition: all 0.5s ease;
-    scale:80%;
-}
-@media screen and (min-width: 521px) {
-    transition: all 0.5s ease;
-    scale:100%; 
-}
-`
-//u are correct popup
-export const PopUpPos2 = styled.div`
-text-align: center;
-display: flex;
-justify-content: center;
-align-items: center;
-text-decoration: none;
-height: 100%;
-width: 100%;
+ @media screen and (max-width: 520px) {
+     transition: all 0.5s ease;
+     scale:80%;
+ }
+ @media screen and (min-width: 521px) {
+     transition: all 0.5s ease;
+     scale:100%; 
+ }
+ `
+ //u are correct popup
+ export const PopUpPos2 = styled.div`
+ text-align: center;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ text-decoration: none;
+ height: 100%;
+ width: 100%;
 
-`
+ `
 
-export const Text = styled.p`
-    font-size:17px;
-    text-align: center;
-    color:#49aa7d;
-`
-export const Close = styled.span`
-    font-size:40px;
-    text-align: center;
-display: flex;
-justify-content: center;
-align-items: center;
-text-decoration: none;
-padding-top: 20px;
-height: 100%;
-width: 100%;
-border: none;
-outline: none;
+ export const Text = styled.p`
+     font-size:17px;
+     text-align: center;
+     color:#49aa7d;
+ `
+ export const Close = styled.span`
+     font-size:40px;
+     text-align: center;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ text-decoration: none;
+ padding-top: 20px;
+ height: 100%;
+ width: 100%;
+ border: none;
+ outline: none;
+ color:#fff;
 
-    &:hover{
-        color:#2d2f66;
-        cursor: pointer;
-    }
+     &:hover{
+        opacity:60%;
+         cursor: pointer;
+     }
 `
